647005aff342ef87312b05f8d5b9d09f0ed21739
Leevi Ossi <leevi.ossi@hotmail.com>
2021-09-03 13:07:02 +0300

    component structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-section">
+        LOGO
+      </div>
+      <div className="navbar-section">
+        LINKS
+      </div>
+      <div className="navbar-section">
+        LANG SOCIALS
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
