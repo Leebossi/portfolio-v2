96c9e0535279a3faa171e20fd04aeeac8953ac0b
Leevi Ossi <leevi.ossi@hotmail.com>
2021-11-01 12:10:47 +0200

    under construction

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import About from './components/About'
-import Contact from './components/Contact'
-import Navbar from './components/Navbar'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
+import UnderConstruction from './components/UnderConstruction';
+// import About from './components/About'
+// import Contact from './components/Contact'
+// import Navbar from './components/Navbar'
+// import Projects from './components/Projects'
+// import Skills from './components/Skills'
 
 const App = () => {
   return (
     <div className="App">
       <main>
-        <Navbar />
+        {/* <Navbar />
         <About />
         <Projects />
         <Skills />
-        <Contact />
+        <Contact /> */}
+        <UnderConstruction />
       </main>
     </div>
   );
---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UnderConstruction = () => {
+  return (
+    <div className="under-construction">
+      <h2>site under construction...</h2>
+    </div>
+  )
+}
+
+export default UnderConstruction
