a1c5b9675a645a53564f4617ea2e302225d8bbcc
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2024-11-28 12:46:54 +0200

    stripe install

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Footer } from "./components/Footer";
-import UnderConstruction from "./components/UnderConstruction";
-
 function App() {
   return (
     <>
-      <main>
-        <UnderConstruction />
-        <Footer />
-      </main>
+      <main></main>
     </>
   );
 }
