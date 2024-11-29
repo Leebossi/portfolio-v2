c7ff4a5bb938445b0fd7ea5fef8a408a54545db7
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2024-11-29 12:37:22 +0200

    stripe payment intent endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,45 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import Stripe from "stripe";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+app.use(express.json());
+
+const calculateOrderAmount = (items: any) => {
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  let total = 0;
+  items.forEach((item: any) => {
+    total += item.amount;
+  });
+  return total;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "eur",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+    // [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
+    dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+  });
+});
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express backend");
