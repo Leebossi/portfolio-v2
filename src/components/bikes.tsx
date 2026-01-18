463892be5ddf270339adb1eb09be4e9f546aa37f
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-18 10:39:19 +0200

    refactor bikeCard into own component and bike tab selector

--- src/components/bikes.tsx
+++ src/components/bikes.tsx
@@ -1,67 +1,34 @@
+import { useState } from "react";
 import bikeData from "../data/bikes.json";
-const bikeComponentsData = bikeData.Peloton;
+import BikeCard from "./BikeCard";
 
-const bikes = () => {
+const Bikes = () => {
+  const bikeEntries = Object.entries(bikeData);
+  const [selectedBikeIndex, setSelectedBikeIndex] = useState(0);
+
   return (
     <div id="bikes">
       <h1>Bikes</h1>
-      <div className="bike-card">
-        {/* image carousel starts here */}
-        <div className="image-carousel">
-          <div className="carousel-title">
-            <h2>Peloton</h2>
-          </div>
-
-          <img
-            src={bikeComponentsData.images[0]}
-            className="bike-img"
-            alt="peloton bike"
-          />
-
-          <div className="carousel-controls">
-            <button className="button-left">{"<"}</button>
-            <ol className="carousel-indicators">
-              <li className="active"></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ol>
-            <button className="button-right">{">"}</button>
-          </div>
-        </div>
-
-        <div className="bike-details">{bikeComponentsData.description}</div>
-        <div className="bike-components">
-          <h3>Components</h3>
-          <ul>
-            {bikeComponentsData.components.map((component, index) => (
-              <li key={index}>
-                {component.name !== "Frame" && <h3>{component.name}</h3>}
-                <table
-                  className={
-                    index === bikeComponentsData.components.length - 1
-                      ? "table-last"
-                      : ""
-                  }
-                >
-                  <tbody>
-                    {Object.entries(component.specs).map(([key, value]) => (
-                      <tr key={key}>
-                        <td className="td-strong">
-                          <strong>{key}:</strong>
-                        </td>
-                        <td>{value}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="bike-container">
+      <div className="bike-tabs">
+        {bikeEntries.map(([bikeKey], index) => (
+          <button
+            key={bikeKey}
+            className={`bike-tab ${selectedBikeIndex === index ? "active" : ""}`}
+            onClick={() => setSelectedBikeIndex(index)}
+          >
+            {bikeKey}
+          </button>
+        ))}
+      </div>
+        <BikeCard
+          key={bikeEntries[selectedBikeIndex][0]}
+          title={bikeEntries[selectedBikeIndex][0]}
+          data={bikeEntries[selectedBikeIndex][1]}
+        />
       </div>
     </div>
   );
 };
 
-export default bikes;
+export default Bikes;
