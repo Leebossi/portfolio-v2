9ff642d97576f93a071c4366a223d3260d7d2fa4
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-18 09:13:03 +0200

    rendering from json

--- src/components/bikes.tsx
+++ src/components/bikes.tsx
@@ -1,4 +1,54 @@
 const bikes = () => {
+  const bikeComponentsData = {
+    description:
+      "The Peloton bike is a high-tech stationary bicycle that offers live and on-demand fitness classes. It features a large touchscreen display, allowing users to stream workouts from the comfort of their own home.",
+    images: [
+      "https://www.yksivaihde.net/uploads/default/original/3X/8/6/867f9bd4b7cdc1c4d14603182a496a7002c2a5a5.jpeg",
+      "https://live.staticflickr.com/65535/52326850789_76f6d3ad27_k.jpg",
+      "https://live.staticflickr.com/65535/52326501636_fc9ab9b6f9_k.jpg",
+      "https://www.yksivaihde.net/uploads/default/original/3X/a/7/a71473e84dd1d8e0a981274c7f22fa89316abea1.jpeg",
+    ],
+    components: [
+      {
+        name: "Frame",
+        specs: {
+          Frame: "53cm NJS Steel",
+        },
+      },
+      {
+        name: "Cockpit & Seating",
+        specs: {
+          Headset: "Tange Levin NJS",
+          Stem: "Nitto CT-02",
+          Handlebars: "Nitto for shred",
+          Grips: "Cotton tape with Nitto bar ends",
+          Saddle: "Selle Italia Flite Titanium",
+          Seatpost: "Nitto S65",
+        },
+      },
+      {
+        name: "Drivetrain",
+        specs: {
+          "Bottom Bracket": "Sugino BB",
+          Crankset: "Sugino 75 DD2 165mm",
+          Chainring: "AARN 53T",
+          Pedals: "Shimano XT",
+          Chain: "Izumi Jet Black",
+          Cog: "EAI 19T / Dura-Ace 14T",
+        },
+      },
+      {
+        name: "Wheels",
+        specs: {
+          "Front Wheel": "Aerospoke",
+          "Rear Hub": "Shimano Dura-Ace 7600",
+          "Rear Rim": "H+SON Archetype 32H",
+          Tires: "Continental GP5000 / Continental Gatorskin",
+        },
+      },
+    ],
+  };
+
   return (
     <div id="bikes">
       <h1>Bikes</h1>
@@ -10,7 +60,7 @@ const bikes = () => {
           </div>
 
           <img
-            src="https://live.staticflickr.com/65535/52326850789_76f6d3ad27_k.jpg"
+            src={bikeComponentsData.images[0]}
             className="bike-img"
             alt="peloton bike"
           />
@@ -27,144 +77,33 @@ const bikes = () => {
           </div>
         </div>
 
-        <div className="bike-details">
-          The Peloton bike is a high-tech stationary bicycle that offers live
-          and on-demand fitness classes. It features a large touchscreen
-          display, allowing users to stream workouts from the comfort of their
-          own home.
-        </div>
+        <div className="bike-details">{bikeComponentsData.description}</div>
         <div className="bike-components">
           <h3>Components</h3>
           <ul>
-            <li>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Frame:</strong>
-                    </td>
-                    <td>53cm NJS Steel</td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <h3>Cockpit & Seating</h3>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Headset:</strong>
-                    </td>
-                    <td>Tange Levin NJS</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Stem:</strong>
-                    </td>
-                    <td>Nitto CT-02</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Handlebars:</strong>
-                    </td>
-                    <td>Nitto for shred</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Grips:</strong>
-                    </td>
-                    <td>Cotton tape with Nitto bar ends</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Saddle:</strong>
-                    </td>
-                    <td>Selle Italia Flite Titanium</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Seatpost:</strong>
-                    </td>
-                    <td>Nitto S65</td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <h3>Drivetrain</h3>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Bottom Bracket:</strong>
-                    </td>
-                    <td>Sugino BB</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Crankset:</strong>
-                    </td>
-                    <td>Sugino 75 DD2 165mm</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Chainring:</strong>
-                    </td>
-                    <td>AARN 53T</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Pedals:</strong>
-                    </td>
-                    <td>Shimano XT</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Chain:</strong>
-                    </td>
-                    <td>Izumi Jet Black</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Cog:</strong>
-                    </td>
-                    <td>EAI 19T / Dura-Ace 14T</td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <h3>Wheels</h3>
-              <table className="table-last">
-                <tbody>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Front Wheel:</strong>
-                    </td>
-                    <td>Aerospoke</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Rear Hub:</strong>
-                    </td>
-                    <td>Shimano Dura-Ace 7600</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Rear Rim:</strong>
-                    </td>
-                    <td>H+SON Archetype 32H</td>
-                  </tr>
-                  <tr>
-                    <td className="td-strong">
-                      <strong>Tires:</strong>
-                    </td>
-                    <td>Continental GP5000 / Continental Gatorskin</td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
+            {bikeComponentsData.components.map((component, index) => (
+              <li key={index}>
+                {component.name !== "Frame" && <h3>{component.name}</h3>}
+                <table
+                  className={
+                    index === bikeComponentsData.components.length - 1
+                      ? "table-last"
+                      : ""
+                  }
+                >
+                  <tbody>
+                    {Object.entries(component.specs).map(([key, value]) => (
+                      <tr key={key}>
+                        <td className="td-strong">
+                          <strong>{key}:</strong>
+                        </td>
+                        <td>{value}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
