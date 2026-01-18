08d32255dd868642b74be7e1544da335ae3aade2
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-18 09:20:58 +0200

    refactored data into separate JSON file

--- src/components/bikes.tsx
+++ src/components/bikes.tsx
@@ -1,54 +1,7 @@
-const bikes = () => {
-  const bikeComponentsData = {
-    description:
-      "The Peloton bike is a high-tech stationary bicycle that offers live and on-demand fitness classes. It features a large touchscreen display, allowing users to stream workouts from the comfort of their own home.",
-    images: [
-      "https://www.yksivaihde.net/uploads/default/original/3X/8/6/867f9bd4b7cdc1c4d14603182a496a7002c2a5a5.jpeg",
-      "https://live.staticflickr.com/65535/52326850789_76f6d3ad27_k.jpg",
-      "https://live.staticflickr.com/65535/52326501636_fc9ab9b6f9_k.jpg",
-      "https://www.yksivaihde.net/uploads/default/original/3X/a/7/a71473e84dd1d8e0a981274c7f22fa89316abea1.jpeg",
-    ],
-    components: [
-      {
-        name: "Frame",
-        specs: {
-          Frame: "53cm NJS Steel",
-        },
-      },
-      {
-        name: "Cockpit & Seating",
-        specs: {
-          Headset: "Tange Levin NJS",
-          Stem: "Nitto CT-02",
-          Handlebars: "Nitto for shred",
-          Grips: "Cotton tape with Nitto bar ends",
-          Saddle: "Selle Italia Flite Titanium",
-          Seatpost: "Nitto S65",
-        },
-      },
-      {
-        name: "Drivetrain",
-        specs: {
-          "Bottom Bracket": "Sugino BB",
-          Crankset: "Sugino 75 DD2 165mm",
-          Chainring: "AARN 53T",
-          Pedals: "Shimano XT",
-          Chain: "Izumi Jet Black",
-          Cog: "EAI 19T / Dura-Ace 14T",
-        },
-      },
-      {
-        name: "Wheels",
-        specs: {
-          "Front Wheel": "Aerospoke",
-          "Rear Hub": "Shimano Dura-Ace 7600",
-          "Rear Rim": "H+SON Archetype 32H",
-          Tires: "Continental GP5000 / Continental Gatorskin",
-        },
-      },
-    ],
-  };
+import bikeData from "../data/bikes.json";
+const bikeComponentsData = bikeData.Peloton;
 
+const bikes = () => {
   return (
     <div id="bikes">
       <h1>Bikes</h1>
