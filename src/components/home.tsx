77c12575e76b231b2f0eec8bfa6024f4ca908886
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-20 10:56:22 +0200

    hero

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,21 +1,29 @@
 const Home = () => {
   return (
-    <>
+    <div id="home">
       <h1>Leevi Ossi</h1>
-      <h3>Software Developer, DIY Dude, Bike Enthusiast</h3>
-      <img
-        src="/src/assets/img/peloton-5.jpg"
-        className="hero-img"
-        alt="leevi"
-      />
-      <div className="hero-text"></div>
-      <p>
-        Welcome to my personal portfolio! I'm Leevi Ossi, a passionate software
-        developer with a love for creating innovative solutions. Here, you'll find
-        information about my projects, skills, and experiences. Feel free to
-        explore and connect with me!
-      </p>
-    </>
+      <div className="container">
+        <div className="image-display">
+          <div className="display-title">
+            <h2>Software Developer, DIY Dude, Bike Enthusiast</h2>
+          </div>
+          <div className="hero-image-container">
+            <img
+              src="/src/assets/img/peloton-5.jpg"
+              className="hero-img"
+              alt="leevi"
+            />
+            <div className="hero-text">
+              <p>
+                Hey! I'm Leevi, a passionate software developer with a love for
+                creating and making things. When I'm not coding, you can find me
+                tinkering with and riding bikes. Welcome to my personal site!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
