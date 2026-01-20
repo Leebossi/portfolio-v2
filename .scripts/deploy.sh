d062934191e1a02dc38c4daf66079fd98d81b83b
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-20 11:46:36 +0200

    deployment scripts

---
+++ .scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+
+# Make sure NVM is available
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Pull the latest version of the app
+git pull origin main
+echo "New changes copied to server !"
+
+# Installing Dependencies
+echo "Installing Dependencies..."
+npm install --yes
+
+# Build 
+echo "Building application"
+npm run build
+
+# Copying dist to /var/www/
+echo "Copying dist to /var/www/personal-site"
+sudo cp -r dist/* /var/www/personal-site
+
+
+
+echo "Deployment Finished!"
