ad390bbcbf9d4b55195fc0f0976d106f1724a79c
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-22 19:28:41 +0200

    translations init with i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import { NavLink, Route, Routes } from "react-router";
+import { useTranslation } from "react-i18next";
+import i18n from "./i18n";
 import "./App.css";
 import Home from "./components/Home";
 import Bikes from "./components/Bikes";
 import Work from "./components/Work";
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
       <nav>
         <div className="nav-main">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/work">Work</NavLink>
-          <NavLink to="/bikes">Bikes</NavLink>
+          <NavLink to="/">{t("nav.home")}</NavLink>
+          <NavLink to="/work">{t("nav.work")}</NavLink>
+          <NavLink to="/bikes">{t("nav.bikes")}</NavLink>
         </div>
         <div className="nav-socials">
-          <a href="https://github.com/leebossi" target="_blank" rel="noopener noreferer">GitHub</a>
+          <button
+            onClick={() =>
+              i18n.changeLanguage(i18n.language === "en" ? "fi" : "en")
+            }
+          >
+            {i18n.language === "en" ? "FI" : "EN"}
+          </button>
+          <a
+            href="https://github.com/leebossi"
+            target="_blank"
+            rel="noopener noreferer"
+          >
+            GitHub
+          </a>
         </div>
       </nav>
       <main>
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import "./i18n.ts";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
