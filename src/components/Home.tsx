05eae112b6741d5dca818d2b38b3636d192b5d6c
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-23 21:40:10 +0200

    home translations

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,26 +1,21 @@
-import image from "../assets/img/hero.jpg"
+import { useTranslation } from "react-i18next";
+import image from "../assets/img/hero.jpg";
 
 const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <div id="home">
       <h1>Leevi Ossi</h1>
       <div className="container">
         <div className="image-display">
           <div className="display-title">
-            <h2>Software Developer, DIY Dude, Bike Enthusiast</h2>
+            <h2>{t("home.title")}</h2>
           </div>
           <div className="hero-image-container">
-            <img
-              src={image}
-              className="hero-img"
-              alt="leevi"
-            />
+            <img src={image} className="hero-img" alt="leevi" />
             <div className="hero-text">
-              <p>
-                Hey! I'm Leevi, a passionate software developer with a love for
-                creating and making things. When I'm not coding, you can find me
-                tinkering with and riding bikes. Welcome to my personal site!
-              </p>
+              <p>{t("home.description")}</p>
             </div>
           </div>
         </div>
